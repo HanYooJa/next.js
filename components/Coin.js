f897200f30b8b19cc184068921f6cd23e44adf7e
HanYooJa <hyj03250@naver.com>
2022-10-20 15:29:39 +0900

    first commit

---
+++ components/Coin.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Coin({ coin }) {
+  return (
+    <div>
+      <Image src={coin.icon} alt={coin.name} width={100} height={100} />
+      <h3>{coin.name}</h3>
+      <p>{coin.price}</p>
+    </div>
+  )
+}
